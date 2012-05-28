8a4840eca7045108970df44cd5921f2ad99b5c25
junald <junaldcl@yahoo.com>
2012-05-28 17:27:27 +0800

    Initial codes for project payroll

---
+++ src/main/java/com/jcl/wrollyap/App.java
@@ -0,0 +1,13 @@
+package com.jcl.wrollyap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
