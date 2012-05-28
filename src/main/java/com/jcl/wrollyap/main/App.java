4c96c336919118a9a9788ba69d3cfbce9bb70bea
junald <junaldcl@yahoo.com>
2012-05-28 18:03:56 +0800

    added xml to support spring framework

---
+++ src/main/java/com/jcl/wrollyap/main/App.java
@@ -0,0 +1,21 @@
+package com.jcl.wrollyap.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    
+     static ApplicationContext context;
+      
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+         context =   new ClassPathXmlApplicationContext("/applicationContext.xml");
+    }
+}
