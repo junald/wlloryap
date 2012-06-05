1149f6d3cd2e3b0b2460a7bcf67410bd6874c9e4
junald <junaldcl@yahoo.com>
2012-06-06 04:03:03 +0800

    update 6/6

---
+++ src/main/resources/sql/default.sql
@@ -0,0 +1,22 @@
+use wlloryap2;
+INSERT INTO branch (code, description) 
+	VALUES ('LOCAL', 'Local');
+
+INSERT INTO department (description) 
+	VALUES ('Administration');
+INSERT INTO department (description) 
+	VALUES ('Transport');
+
+
+INSERT INTO position (description) 
+	VALUES ('Cashier');
+INSERT INTO position (description) 
+	VALUES ('Driver');
+
+
+INSERT INTO company (address, description, `telephoneNo`, zipcode) 
+	VALUES (NULL, 'HJR International Corp.', NULL, NULL);
+
+INSERT INTO companysetting (address, description, `telephoneNo`, zipcode) 
+	VALUES (NULL, 'HJR International Corp.', NULL, NULL);
+
