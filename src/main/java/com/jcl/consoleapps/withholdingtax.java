285e1f7bf423caca3b78c3f1ff69e8a289111cc6
Kent Lavador <kclavador2011@yaho.com>
2012-06-07 00:22:43 +0800

    trying to encode withholdingtax.java

---
+++ src/main/java/com/jcl/consoleapps/withholdingtax.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jcl.consoleapps;
+
+import com.jcl.payroll.data.WithHoldingTax;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Kent
+ */
+public class withholdingtax {
+    
+    public List<WithHoldingTax> whtList = new ArrayList<WithHoldingTax>();
+    
+  //  public WithHoldingTax getWithHoldingTax (Double Salary) {
+        WithHoldingTax wht = null;
+    //    for (WithHoldingTax whte : whtList) {
+ 
+    //}
+}
