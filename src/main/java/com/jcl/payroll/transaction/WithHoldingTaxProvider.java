82d5e18a535db45a1baafc297c8c7f183edff9b6
junald <junaldcl@yahoo.com>
2012-06-17 11:45:25 +0800

    taxtable

--- src/main/java/com/jcl/payroll/transaction/WithHoldingTaxProvider.java
+++ src/main/java/com/jcl/payroll/transaction/WithHoldingTaxProvider.java
@@ -6,6 +6,8 @@ package com.jcl.payroll.transaction;
 
 import com.jcl.payroll.data.WithHoldingTax;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 /**
@@ -14,7 +16,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Li
  */
-/*
+ 
 public class WithHoldingTaxProvider {
 
     private static List<WithHoldingTax> withHoldingTaxList = null;
@@ -37,81 +39,75 @@ public class WithHoldingTaxProvider {
         return wht;    
     }
     
-    public static void init(){
-        withHoldingTaxList = new ArrayList<WithHoldingTax>();
-//        withHoldingTaxList.add(new WithHoldingTax(0,4999.99,4000,100,50,50));
-        
-        
-    }
+   
             
     // test 
    //     WithHoldingTax taxType = null; 
-   /* if (taxType.getTaxID() == "Z"){
-        if(salary >= )
-    }
+//   if (taxType.getTaxID() == "Z"){
+//        if(salary >= )
+//    }
     
-    type z
-  col1(0, 1, 0.0, 0.0);
-  col2(0, 416, 0.0, 0.05);
-  col3(417, 1249, 20.83, 0.1);
-  col4(1250, 2916, 104.17, 0.15);
-  col5(2917, 5832, 354.17, 0.2);
-  col6(5833, 10416, 937.5, 0.25);
-  col7(10417, 20832, 2083.33, 0.3);
-  col8(20833, 50000, 5208.33, 0.32);
+//    type z
+//  col1(0, 1, 0.0, 0.0);
+//  col2(0, 416, 0.0, 0.05);
+//  col3(417, 1249, 20.83, 0.1);
+//  col4(1250, 2916, 104.17, 0.15);
+//  col5(2917, 5832, 354.17, 0.2);
+//  col6(5833, 10416, 937.5, 0.25);
+//  col7(10417, 20832, 2083.33, 0.3);
+//  col8(20833, 50000, 5208.33, 0.32);
+//   
+//  type s/hf/me
+//  col1(1, 2082, 0.0, 0.0);
+//  col2(2083, 2499, 0.0, 0.05);
+//  col3(2500, 3332, 20.83, 0.1);
+//  col4(3333, 4999, 104.17, 0.15);
+//  col5(5000, 7916, 345.17, 0.2);
+//  col6(7917, 12499, 937.5, 0.25);
+//  col7(12500, 22,916, 2083.33, 0.3);
+//  col8(22,917, 50000, 5208.33, 0.32);
+//   
+//  type me1/hf1
+//  col1(1, 3124, 0.0, 0.0);
+//  col2(3125, 3541, 0.0, 0.05);
+//  col3(3542, 4374, 20.83, 0.1);
+//  col4(4375, 6041, 104.17, 0.15);
+//  col5(6042, 8957, 354.17, 0.2);
+//  col6(8958, 13541, 937.5, 0.25);
+//  col7(13542, 23957, 2083.33, 0.3);
+//  col8(23958, 50000, 5208.33, 0.32);
+//     
+//  type me2/hf2
+//  col1(1, 4166, 0.0, 0.0);
+//  col2(4167, 4582, 0.0, 0.05);
+//  col3(4583, 5416, 20.83, 0.1);
+//  col4(5417, 7082, 104.17, 0.15);
+//  col5(7083, 9999, 354.17, 0.2);
+//  col6(10000, 14582, 937.5, 0.25);
+//  col7(14583, 24999, 2083.33, 0.30);
+//  col8(25000, 50000, 5208.33, 0.32);
+//  
+//  type me3/hf3
+//  col1(1, 5207, 0.0, 0.0);
+//  col2(5208, 5624, 0.0, 0.05);
+//  col3(5625, 6457, 20.83, 0.1);
+//  col4(6458, 8124, 104.17, 0.15);
+//  col5(8125, 11041, 354.17, 0.2);
+//  col6(11042, 15624, 937.5, 0.25);
+//  col7(15625, 26041, 2083.33, 0.3);
+//  col8(26042, 50000, 5208.33, 0.32);
+//   
+//  type me4/hf4
+//  col1(1, 6249, 0.0, 0.0);
+//  col2(6250, 6666, 0.0, 0.05);
+//  col3(6667, 7499, 20.83, 0.1);
+//  col4(7500, 9166, 104.17, 0.15);
+//  col5(9167, 12082, 354.17, 0.2);
+//  col6(12083, 16666, 937.5, 0.25);
+//  col7(16667, 27082, 2083.33, 0.3);
+//  col8(27083, 50000, 5208.33, 0.32);
    
-  type s/hf/me
-  col1(1, 2082, 0.0, 0.0);
-  col2(2083, 2499, 0.0, 0.05);
-  col3(2500, 3332, 20.83, 0.1);
-  col4(3333, 4999, 104.17, 0.15);
-  col5(5000, 7916, 345.17, 0.2);
-  col6(7917, 12499, 937.5, 0.25);
-  col7(12500, 22,916, 2083.33, 0.3);
-  col8(22,917, 50000, 5208.33, 0.32);
-   
-  type me1/hf1
-  col1(1, 3124, 0.0, 0.0);
-  col2(3125, 3541, 0.0, 0.05);
-  col3(3542, 4374, 20.83, 0.1);
-  col4(4375, 6041, 104.17, 0.15);
-  col5(6042, 8957, 354.17, 0.2);
-  col6(8958, 13541, 937.5, 0.25);
-  col7(13542, 23957, 2083.33, 0.3);
-  col8(23958, 50000, 5208.33, 0.32);
-     
-  type me2/hf2
-  col1(1, 4166, 0.0, 0.0);
-  col2(4167, 4582, 0.0, 0.05);
-  col3(4583, 5416, 20.83, 0.1);
-  col4(5417, 7082, 104.17, 0.15);
-  col5(7083, 9999, 354.17, 0.2);
-  col6(10000, 14582, 937.5, 0.25);
-  col7(14583, 24999, 2083.33, 0.30);
-  col8(25000, 50000, 5208.33, 0.32);
-  
-  type me3/hf3
-  col1(1, 5207, 0.0, 0.0);
-  col2(5208, 5624, 0.0, 0.05);
-  col3(5625, 6457, 20.83, 0.1);
-  col4(6458, 8124, 104.17, 0.15);
-  col5(8125, 11041, 354.17, 0.2);
-  col6(11042, 15624, 937.5, 0.25);
-  col7(15625, 26041, 2083.33, 0.3);
-  col8(26042, 50000, 5208.33, 0.32);
-   
-  type me4/hf4
-  col1(1, 6249, 0.0, 0.0);
-  col2(6250, 6666, 0.0, 0.05);
-  col3(6667, 7499, 20.83, 0.1);
-  col4(7500, 9166, 104.17, 0.15);
-  col5(9167, 12082, 354.17, 0.2);
-  col6(12083, 16666, 937.5, 0.25);
-  col7(16667, 27082, 2083.33, 0.3);
-  col8(27083, 50000, 5208.33, 0.32);
-   
-   
-    public static void main(String[] args){
+   public static void main(String[] args){
             System.out.println("withholdingtax contribution.");
             WithHoldingTaxProvider wht =new WithHoldingTaxProvider();
             wht.init();
@@ -121,6 +117,46 @@ public class WithHoldingTaxProvider {
 //            System.out.println(whte.getEr());
             
     }
+  public static HashMap<String, ArrayList<TaxData>> taxTable ;
+  
+   public static void init(){
+//        withHoldingTaxList = new ArrayList<WithHoldingTax>();
+//        withHoldingTaxList.add(new WithHoldingTax(0,4999.99,4000,100,50,50));
+       
+       taxTable  = new LinkedHashMap<String, ArrayList<TaxData>>();
+       ArrayList<TaxData>  zList = new ArrayList<TaxData>();
+       zList.add(new TaxData(0, 1, 0.0, 0.0));
+        zList.add(new TaxData(0, 416, 0.0, 0.05));
+        taxTable.put("Z", zList);
+        
+        //taxTable.put("S_ME",smeList);
+        
+        //taxTable.get("S_ME");
+        
+//  col1(0, 1, 0.0, 0.0);
+//  col2(0, 416, 0.0, 0.05);
+//  col3(417, 1249, 20.83, 0.1);
+//  col4(1250, 2916, 104.17, 0.15);
+//  col5(2917, 5832, 354.17, 0.2);
+//  col6(5833, 10416, 937.5, 0.25);
+//  col7(10417, 20832, 2083.33, 0.3);
+//  col8(20833, 50000, 5208.33, 0.32);
+//   
+        
+        
+    }
     
 }
-*/
+ class TaxData{
+       double range1;
+       double range2;
+       double over;
+       double due;
+       public TaxData(double range1, double range2, double over, double due){
+           this.range1 = range1;
+           this.range2 = range2;
+           this.over = over;
+           this.due = due;           
+       }
+       
+   }
